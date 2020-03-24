70edd7ac58be729c52fdc8f74457ac9404f96edd
Douglas Marcelino <douglas_champion@hotmail.com>
2020-03-24 20:45:35 -0300

    Cria api OngController
    -listar ongs
    -criar ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,21 +1,9 @@
 const express = require('express');
-
 const routes = express.Router();
 
-/**
- * Tipos de parâmetros:
- * Query params - parâmetros nomeados enviados na rota após "?" (filtro, paginação)
- * Route params - parâmetros utilizados para identificar recursos
- * Request body - corpo da requisição, utilizado para criar ou alterar recursos
- */
+const OngController = require('./controllers/OngController');
 
-routes.post('/users', (request, response) => {
-    const params = request.body;
-    console.log(params);
-    return response.json({
-        evento: 'Semana OmniStack 11.0',
-        aluno: 'Douglas Alves Marcelino'
-    });
-})
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
 
 module.exports = routes;
