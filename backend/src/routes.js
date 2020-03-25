3550b0947ca1ed23a68f154ee2e5486da36cd078
Douglas Marcelino <douglas_champion@hotmail.com>
2020-03-24 21:31:08 -0300

    Cria IncidentController e suas rotas
    -listar
    -criar
    -deletar

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,14 @@ const express = require('express');
 const routes = express.Router();
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
-routes.get('/ongs', OngController.index)
-routes.post('/ongs', OngController.create)
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
