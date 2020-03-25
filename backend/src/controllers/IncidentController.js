96b0696953ad6faa5ccb9b7a00c17e9db7172ac5
Douglas Marcelino <douglas_champion@hotmail.com>
2020-03-24 22:05:51 -0300

    Joina ONGs junto com o incidents (em index)

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -9,9 +9,17 @@ module.exports = {
         const [count] = await connection('incidents').count();
     
         const incidents = await connection('incidents')
+            .join('ongs', 'ong_id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
-            .select('*'); 
+            .select([
+                'incidents.*',
+                 'ongs.name', 
+                 'ongs.email', 
+                 'ongs.whatsapp', 
+                 'ongs.city', 
+                 'ongs.uf'
+            ]); 
 
         response.header('X-Total-Count', count['count(*)']);
     
