506aede65a682131fa6068ca45e6aef9c69c2027
Douglas Marcelino <douglas_champion@hotmail.com>
2020-03-24 21:32:18 -0300

    Cria ProfileController
    get: exibirá todos os incidentes de uma ONG

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -12,4 +12,6 @@ routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 
+routes.get('/profile', ProfileController.index);
+
 module.exports = routes;
