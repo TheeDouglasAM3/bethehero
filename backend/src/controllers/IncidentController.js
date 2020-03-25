dc7fe5dc3f3632e1aa645405827a24492dfce58e
Douglas Marcelino <douglas_champion@hotmail.com>
2020-03-24 21:50:22 -0300

    Altera IncidentController:index (add paginação)

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,8 +3,17 @@ const connection = require('../database/connection');
 module.exports = {
 
     async index(request, response) {
+
+        const { page = 1 } = request.query;
+
+        const [count] = await connection('incidents').count();
     
-        const incidents = await connection('incidents').select('*'); 
+        const incidents = await connection('incidents')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select('*'); 
+
+        response.header('X-Total-Count', count['count(*)']);
     
         return response.json(incidents);
     },
