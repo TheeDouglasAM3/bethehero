bf7ed89508729c793d02df916809d7db24a87752
Douglas Marcelino <douglas_champion@hotmail.com>
2020-03-26 02:51:06 -0300

    Ajusta retorno em ProfileController:index

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -10,6 +10,6 @@ module.exports = {
             .where('ong_id', ong_id)
             .select('*');
 
-        return response.json({ incidents });
+        return response.json( incidents );
     }
 }
