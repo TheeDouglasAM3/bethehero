488444127ad5c2195bb08e46143173b2c9e349f8
Douglas Marcelino <douglas_champion@hotmail.com>
2020-03-26 00:55:37 -0300

    Cria página de Registro

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function Register(){
+
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero"/>
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem casos de sua ONG.</p>
+                    <Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="#E02041"/>
+                        Não tenho cadastro
+                    </Link>
+                </section>
+                <form>
+                    <input type="text" placeholder="Nome da ONG"/>
+                    <input type="email" placeholder="E-mail"/>
+                    <input type="text" placeholder="WhatsApp"/>
+                    <div className="input-group">
+                        <input type="text" placeholder="Cidade"/>
+                        <input type="text" placeholder="UF" style={{ width: 80 }} />
+                    </div>
+                    <button className="button">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    );
+}
