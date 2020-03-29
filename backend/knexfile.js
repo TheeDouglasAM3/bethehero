54e183f628e9df3589c4b3c6c658f2eb19f1d88e
Douglas Marcelino <douglas_champion@hotmail.com>
2020-03-28 22:02:00 -0300

    Cria e configura banco de dados pra testes

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -13,6 +13,17 @@ module.exports = {
     useNullAsDefault: true
   },
 
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/dbtest.sqlite'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true
+  },
+
   staging: {
     client: 'postgresql',
     connection: {
